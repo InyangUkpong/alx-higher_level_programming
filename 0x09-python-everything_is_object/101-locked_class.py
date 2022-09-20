5e5c3aadbf9ef2c48730a3f9800adf1b040097cc
Inyang Ukpong <inyangweb@gmail.com>
2022-09-20 20:43:59 +0100

    edited 101-loced_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""blocked class module"""
-
-
 class LockedClass:
-    """object prevents dynamic attribute"""
 
     __slots__ = ['first_name']
+
+    def __init__(self, first_name=''):
+        self.first_name = first_name
